9bbbef7e99cf89ea374a3651c7f33a5a0d052b2e
David Gordon <davidgordondev@gmail.com>
2025-03-24 17:27:56 -0400

    Add proper forwarding to different endpoints

--- lb.go
+++ lb.go
@@ -4,14 +4,10 @@ import (
 	"bufio"
 	"net"
 	"net/http"
+	"strings"
 )
 
 var serverIndex = 0
-var serverPool = []string{
-	"http://127.0.0.1:5001",
-	"http://127.0.0.1:5002",
-	"http://127.0.0.1:5003",
-}
 
 func Serve() {
 	listener, err := net.Listen("tcp", "localhost:80")
@@ -49,15 +45,15 @@ func handleClient(conn net.Conn) {
 
 	audit.info((string)(buff[:size]))
 
-	resp := forwardRequest()
+	resp := forwardRequest(strings.Split(string(buff), " ")[1])
 
 	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 	conn.Write(resp)
 }
 
-func forwardRequest() []byte {
+func forwardRequest(endpoint string) []byte {
 	serverIndex = (serverIndex + 1) % len(serverPool)
-	resp, err := http.Get(serverPool[serverIndex])
+	resp, err := http.Get(serverPool[serverIndex] + endpoint)
 	if err != nil {
 		audit.error("Couldn't forward request " + err.Error())
 		return nil
@@ -66,10 +62,8 @@ func forwardRequest() []byte {
 	buff := make([]byte, 32000)
 	httpReader := bufio.NewReader(resp.Body)
 	size, err := httpReader.Read(buff)
-	if err != nil {
-		audit.error("Couldn't read request " + err.Error())
-		return nil
+	if err != nil && err.Error() != "EOF" {
+		audit.error("Couldn't read response " + err.Error())
 	}
-
 	return buff[:size]
 }
