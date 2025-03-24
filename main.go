928b3a9c2c97bce156342cd5868eb8b9d674099d
David Gordon <davidgordondev@gmail.com>
2025-03-24 17:27:27 -0400

    Add servers to server pool

--- main.go
+++ main.go
@@ -6,5 +6,13 @@ func main() {
 	// Init audit
 	audit = NewAudit()
 	audit.addFile("logs.txt")
+
+	// Init server pool
+	addServer("http://127.0.0.1:5001", &serverPool)
+	addServer("http://127.0.0.1:5002", &serverPool)
+	addServer("http://127.0.0.1:5003", &serverPool)
+
+	go StartHealthcheckService()
+
 	Serve()
 }
